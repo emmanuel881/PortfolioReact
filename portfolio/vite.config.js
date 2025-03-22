8736a70aa0a1a26f5de056186535e2e4f4569a65
emmanuel <emmanuelsaura008@gmail.com>
2025-03-22 14:06:23 +0300

    my commit

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: './',  // Ensures assets load correctly on Vercel
-});
+  plugins: [react(), tailwindcss()],
+  base: "./" //its to enuse it loads properly in vercel
+
+
+})
