4f4e9282db852e12d1262c3af334063f069c5938
emmanuel <emmanuelsaura008@gmail.com>
2025-03-22 14:34:38 +0300

    my commit

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "./" //its to enuse it loads properly in vercel
+  base: "/" //its to enuse it loads properly in vercel
 
 
 })
